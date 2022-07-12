4d1432e930beb930f570b97dd41e410c70b7f4a4
rahulvirani03 <rahulsvirani25@gmail.com>
2022-07-12 16:46:37 +0530

    Port changes

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = 8080;
+
 //DB Connection
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.listen(PORT, console.log(`Server running on PORT ${PORT}`));
+app.listen(
+  process.env.PORT || 5000,
+  console.log(`Server running on PORT ${process.env.PORT}`)
+);
 //Routes
 app.get("/resume-download", (req, res) => {
   res.download(`${process.cwd()}/resume.pdf`);
